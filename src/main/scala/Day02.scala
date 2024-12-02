2bc9ccd5fc6d88cbad37073bf6fbad2a49ed8bfb
Anna Yudina <annayudi@gmail.com>
2024-12-02 11:16:52 +0100

    Day 2

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,18 +1,41 @@
 import Util.readFile
 
+import scala.annotation.tailrec
+import scala.collection.immutable.::
+
 @main def day02(): Unit = {
 
   val input = readFile("resources/day02")
 
   // Part 1
 
-  val result1 = "foo"
+  val reports = input.map { line =>
+    line.split("\\s+").map(_.toInt).toList
+  }
+
+  val result1 = reports.count(isSafe(_))
 
   println(result1)
 
   // Part 2
 
-  val result2 = "foo"
+  val result2 = reports.count { report =>
+    generateStates(report).exists(isSafe(_))
+  }
 
   println(result2)
 }
+
+@tailrec
+def isSafe(xs: List[Int], direction: Int = 0, acc: Boolean = true): Boolean =
+  (xs, direction, acc) match
+    case (_, _, false)                  => false
+    case (x :: y :: ys, 0, _) if x == y => false
+    case (x :: y :: ys, 0, _)           => isSafe(xs, (y - x) / (y - x).abs)
+    case (x :: y :: ys, 1, _)           => isSafe(y :: ys, 1, y >= x + 1 && y <= x + 3)
+    case (x :: y :: ys, -1, _)          => isSafe(y :: ys, -1, y <= x - 1 && y >= x - 3)
+    case _                              => true
+
+def generateStates(xs: List[Int]): List[List[Int]] = xs :: xs.indices.map { i =>
+  xs.take(i) ++ xs.drop(i + 1)
+}.toList
