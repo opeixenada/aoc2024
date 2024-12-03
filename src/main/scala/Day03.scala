37fb22a87f635d37bf1a93a122463cda58bb167e
Anna Yudina <annayudi@gmail.com>
2024-12-03 11:24:00 +0100

    Day 3

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -6,13 +6,37 @@ import Util.readFile
 
   // Part 1
 
-  val result1 = "foo"
+  val result1 = input.flatMap { line =>
+    """mul\(\d+,\d+\)""".r
+      .findAllIn(line)
+      .toList
+      .map { exp =>
+        """\d+""".r.findAllIn(exp).map(_.toInt).product
+      }
+  }.sum
 
   println(result1)
 
   // Part 2
 
-  val result2 = "foo"
+  val result2 = input
+    .flatMap { line =>
+      """mul\(\d+,\d+\)|do\(\)|don't\(\)""".r
+        .findAllIn(line)
+        .toList
+    }
+    .foldLeft((List.empty[String], true)) { case ((xs, flag), s) =>
+      s match
+        case "don't()" => (xs, false)
+        case "do()"    => (xs, true)
+        case _ if flag => (s :: xs, flag)
+        case _         => (xs, flag)
+    }
+    ._1
+    .map { exp =>
+      """\d+""".r.findAllIn(exp).map(_.toInt).product
+    }
+    .sum
 
   println(result2)
 }
