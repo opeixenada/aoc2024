3069037be1d99d8d6cdae5338c5a002ddc5c80a4
Anna Yudina <annayudi@gmail.com>
2024-12-13 17:40:35 +0100

    Day 13.1

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -1,13 +1,35 @@
+import ClawContraption.Machine
 import Util.readFile
 
 @main def day13(): Unit = {
   val input = readFile("resources/day13")
-  val solver = Foo13(input)
+  val solver = ClawContraption(input)
   println(solver.solvePart1())
   println(solver.solvePart2())
 }
 
-class Foo13(input: List[String]) {
-  def solvePart1(): Any = ???
+object ClawContraption {
+  private val aPrice = 3
+  private val bPrice = 1
+  private val maxCount = 100
+
+  case class Machine(ax: Int, ay: Int, bx: Int, by: Int, px: Int, py: Int) {
+    def getMinTokens: Option[Int] = (for {
+      a <- 0 to 100
+      if (px - (a * ax)) % bx == 0
+      b = (px - (a * ax)) / bx
+      if py == a * ay + b * by
+    } yield (a * 3) + b).minOption
+  }
+}
+
+class ClawContraption(input: List[String]) {
+
+  private val machines: Iterable[Machine] = Util.splitBy(input, _.isBlank).map { m =>
+    val ns = "(\\d)+".r.findAllMatchIn(m.mkString("\n")).map(_.matched.toInt).toIndexedSeq
+    Machine(ns(0), ns(1), ns(2), ns(3), ns(4), ns(5))
+  }
+
+  def solvePart1(): Any = machines.flatMap(_.getMinTokens).sum
   def solvePart2(): Any = ???
 }
