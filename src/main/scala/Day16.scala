1a66bb5759b69021c2df2ccd57a6ae26c327658c
Anna Yudina <annayudi@gmail.com>
2024-12-17 14:38:25 +0100

    Day 16.2

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -1,5 +1,5 @@
 import ReindeerMaze.{Maze, Point, State, solve}
-import Util.{coordinatesOf, readFile, withTimeLogging}
+import Util.{coordinatesOf, readFile}
 
 import scala.annotation.tailrec
 
@@ -11,25 +11,27 @@ import scala.annotation.tailrec
 }
 
 object ReindeerMaze {
-  type Point = (Int, Int)
-  type DirectedPoint = (Point, Char)
-  type Path = Set[Point]
+  private type Point = (Int, Int)
+  private type DirectedPoint = (Point, Char)
+  private type Path = Set[Point]
 
-  val up: Point => DirectedPoint = p => (p._1, p._2 - 1) -> '^'
-  val down: Point => DirectedPoint = p => (p._1, p._2 + 1) -> 'v'
-  val left: Point => DirectedPoint = p => (p._1 - 1, p._2) -> '<'
-  val right: Point => DirectedPoint = p => (p._1 + 1, p._2) -> '>'
+  private val allDirections : List[Char] = List('v', '^', '<', '>')
 
-  val allMoves = Set(up, down, left, right)
+  private val up: Point => DirectedPoint = p => (p._1, p._2 - 1) -> '^'
+  private val down: Point => DirectedPoint = p => (p._1, p._2 + 1) -> 'v'
+  private val left: Point => DirectedPoint = p => (p._1 - 1, p._2) -> '<'
+  private val right: Point => DirectedPoint = p => (p._1 + 1, p._2) -> '>'
 
-  val oppositeMove = Map(
+  private val allMoves = Set(up, down, left, right)
+
+  private val oppositeMove = Map(
     '^' -> down,
     '<' -> right,
     '>' -> left,
     'v' -> up
   )
 
-  def getMoves(direction: Char) = allMoves - oppositeMove(direction)
+  private def getMoves(direction: Char) = allMoves - oppositeMove(direction)
 
   case class Maze(walls: Set[Point], end: Point, w: Int, h: Int) {
     def isValid(point: Point): Boolean =
@@ -54,54 +56,48 @@ object ReindeerMaze {
       maze: Maze
   )(
       states: List[State],
-      scores: Map[Point, Int] = Map.empty.withDefaultValue(Int.MaxValue),
+      scores: Map[DirectedPoint, Int],
       paths: Set[(Path, Int)] = Set.empty
-  ): (Int, Path) =
-    states match
-      case state :: tail if scores(state.point) < state.pathScore => solve(maze)(tail, scores, paths)
+  ): (Int, Path) = states match
+    case state :: tail =>
+      val currentScore = scores(state.point -> state.direction)
+      val newScore = Math.min(currentScore, state.pathScore)
+      val updatedScores = scores + (state.point -> state.direction -> newScore)
 
-      case state :: tail if state.point == maze.end =>
-        val newMinScore = Math.min(scores(state.point), state.pathScore)
-        val updatedScores = scores + (state.point -> newMinScore)
-        val newStates = tail.filter(_.pathScore <= newMinScore)
-        val newPaths = paths + (state.path -> state.pathScore)
-        solve(maze)(newStates, updatedScores, newPaths)
+      () match
+        case _ if currentScore < state.pathScore =>
+          solve(maze)(tail, scores, paths)
 
-      case state :: tail =>
-        val newMinScore = Math.min(scores(state.point), state.pathScore)
-        val updatedScores = scores + (state.point -> newMinScore)
-        val newStates = (tail ::: state.next(maze)).filter { state =>
-          state.pathScore <= updatedScores(state.point)
-        }
-        solve(maze)(newStates, updatedScores, paths)
+        case _ if state.point == maze.end =>
+          solve(maze)(
+            states = tail.filter(_.pathScore <= newScore),
+            scores = updatedScores,
+            paths = paths + (state.path -> state.pathScore)
+          )
 
-      case Nil =>
-        val finalScore = scores(maze.end)
-        finalScore -> paths.filter(_._2 == finalScore).flatMap(_._1)
+        case _ =>
+          solve(maze)(
+            states = (tail ::: state.next(maze)).filter(s => s.pathScore <= updatedScores(s.point -> s.direction)),
+            scores = updatedScores,
+            paths = paths
+          )
+
+    case Nil =>
+      val finalScore = allDirections .map(d => scores(maze.end -> d)).min
+      finalScore -> paths.filter(_._2 == finalScore).flatMap(_._1)
 }
 
 class ReindeerMaze(input: List[String]) {
 
-  val walls: Set[Point] = coordinatesOf('#', input).toSet
-  val start: Point = coordinatesOf('S', input).head
-  val end: Point = coordinatesOf('E', input).head
+  private val walls: Set[Point] = coordinatesOf('#', input).toSet
+  private val start: Point = coordinatesOf('S', input).head
+  private val end: Point = coordinatesOf('E', input).head
 
-  val maze = Maze(walls, end, input.head.length, input.length)
-  val state0 = State(start, '>', Set(start))
+  private val maze = Maze(walls, end, input.head.length, input.length)
+  private val state0 = State(start, '>', Set(start))
 
-  val (score, path) = solve(maze)(List(state0))
-
-  (0 until maze.h).foreach { y =>
-    println(
-      (0 until maze.w).map { x =>
-        if (path.contains(x, y)) 'O'
-        else if (start == (x, y)) 'S'
-        else if (end == (x, y)) 'E'
-        else if (walls.contains(x, y)) '#'
-        else '.'
-      }.mkString
-    )
-  }
+  private val (score, path) =
+    solve(maze)(List(state0), Map(state0.point -> state0.direction -> 0).withDefaultValue(Int.MaxValue))
 
   def solvePart1(): Any = score
   def solvePart2(): Any = path.size
