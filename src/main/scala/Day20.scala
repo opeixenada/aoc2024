467f50340863a09216523bb8e0473d594bee443c
Anna Yudina <annayudi@gmail.com>
2024-12-22 19:29:28 +0100

    Day 20.2

--- src/main/scala/Day20.scala
+++ src/main/scala/Day20.scala
@@ -12,57 +12,52 @@ import scala.annotation.tailrec
 
 object RaceCondition {
 
-  private val psToSave: Int = 2
-
   private val directions: List[(Int, Int)] = List((0, 1), (1, 0), (0, -1), (-1, 0))
 
-  case class Maze(walls: Set[Point], end: Point, w: Int, h: Int) {
-    def isValid(point: Point): Boolean =
+  case class Maze(walls: Set[Point], start: Point, end: Point, w: Int, h: Int) {
+    val path: Map[Point, Int] = getPathsRec(List(State(start))).zipWithIndex.toMap
+
+    private def isValid(point: Point): Boolean =
       point._1 > -1 && point._1 < w && point._2 > -1 && point._2 < h && !walls.contains(point)
 
-    def isWall(point: Point): Boolean = walls.contains(point)
+    private def isWall(point: Point): Boolean = walls.contains(point)
 
-    def getPath(start: Point, maxTime: Option[Int] = None): Option[Set[(Int, Int)]] =
-      getPathsRec(List(State(start)), maxTime)
-
-    def next(s: State): List[State] = getNeighbors(s.point)
+    private def next(s: State): List[State] = getNeighbors(s.point)
       .filter(isValid)
-      .map(State(_, s.seen + s.point))
+      .map(State(_, s.point :: s.seen))
 
     @tailrec
     private final def getPathsRec(
         states: List[State],
-        maxTime: Option[Int],
-        seen: Set[Point] = Set.empty
-    ): Option[Set[Point]] = states match
-      case s :: ss if maxTime.exists(_ < s.seen.size) => None
-      case s :: ss if seen.contains(s.point)          => getPathsRec(ss, maxTime, seen)
-      case s :: ss if s.point == end                  => Some(s.seen)
-      case s :: ss                                    => getPathsRec(ss ::: next(s), maxTime, seen + s.point)
-      case Nil                                        => None
+        seen: List[Point] = Nil
+    ): List[Point] = states match
+      case s :: ss if seen.contains(s.point) => getPathsRec(ss, seen)
+      case s :: ss if s.point == end         => (s.point :: s.seen).reverse
+      case s :: ss                           => getPathsRec(ss ::: next(s), s.point :: seen)
+      case Nil                               => throw Exception("Path not found")
 
-    private def getPossibleCheats(path: Set[Point]): Set[Point] = walls.filter { wall =>
-      getNeighbors(wall).toSet.intersect(path + end).size > 1
-    }
+    def countCheats(psToSave: Int, cheatLength: Int): Int = {
+      val cheats = for {
+        (pathPoint, pathPointIndex) <- path
+        diffX <- (0 - cheatLength) to cheatLength
+        diffY <- (0 - cheatLength) to cheatLength
+        distance = diffX.abs + diffY.abs
+        if distance <= cheatLength
+        cheatDestination = (pathPoint._1 + diffX, pathPoint._2 + diffY)
+        cheatDestinationIndex <- path.get(cheatDestination)
+        cheatWin = cheatDestinationIndex - pathPointIndex - distance
+        if cheatWin >= psToSave
+      } yield ()
 
-    def countGoodCheats(start: Point): Int = {
-      val goodCheats = for {
-        path <- getPath(start).toSeq
-        pathTime = path.size
-        cheat <- getPossibleCheats(path)
-        cheatedMaze = this.copy(walls = walls - cheat)
-        cheatedPath <- cheatedMaze.getPath(start, Some(pathTime - psToSave))
-      } yield cheat
-
-      goodCheats.size
+      cheats.size
     }
   }
 
   private def getNeighbors(point: Point): List[Point] = directions
     .map { case (x, y) => (point._1 + x) -> (point._2 + y) }
 
-  case class State(point: Point, seen: Set[Point] = Set.empty) {
-    val ps: Int = seen.size
+  case class State(point: Point, seen: List[Point] = Nil) {
+    val ps: Int = seen.size + 1
   }
 }
 
@@ -72,8 +67,8 @@ class RaceCondition(input: List[String]) {
   private val start: Point = coordinatesOf('S', input).head
   private val end: Point = coordinatesOf('E', input).head
 
-  private val maze = Maze(walls, end, input.head.length, input.length)
+  private val maze = Maze(walls, start, end, input.head.length, input.length)
 
-  def solvePart1(): Any = maze.countGoodCheats(start)
-  def solvePart2(): Any = ???
+  def solvePart1(): Any = maze.countCheats(psToSave = 100, cheatLength = 2)
+  def solvePart2(): Any = maze.countCheats(psToSave = 100, cheatLength = 20)
 }
